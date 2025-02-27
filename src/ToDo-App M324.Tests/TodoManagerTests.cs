88f1298bb3520d0983df5f1a8c053a1e83ee9c81
Sullivan Renner <renner.sullivan@gmail.com>
2025-02-27 20:26:04 +0100

    Moved from saving Data in a json file to a sqlite-Db

--- src/ToDo-App M324.Tests/TodoManagerTests.cs
+++ src/ToDo-App M324.Tests/TodoManagerTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.Json;
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Reflection;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using NUnit.Framework;
 using ToDo_App_M324.Logic;
@@ -9,7 +12,7 @@ namespace ToDo_App_M324.Tests;
 [NonParallelizable]
 public class TodoManagerTests
 {
-    private const string file = "todo_list.json"; // must be the same as in TodoManager
+    private string file = "todo_list.json"; // must be the same as in TodoManager
     private JsonSerializerOptions options = null!;
 
     [OneTimeSetUp]
@@ -20,6 +23,51 @@ public class TodoManagerTests
             WriteIndented = true
         };
         options.Converters.Add(new JsonStringEnumConverter());
+
+        var prop = typeof(TodoManager).GetField("file", BindingFlags.Static | BindingFlags.NonPublic)!;
+        file = (string)prop.GetValue(null)!;
+
+        // Trigger static constructor
+        TodoManager.RemoveTodo(-1);
+    }
+
+    private void SetupDatasource(int count)
+    {
+        var todos = Enumerable.Range(0, count).Select(i => new Todo
+        {
+            Header = Guid.NewGuid().ToString(),
+            Description = Guid.NewGuid().ToString(),
+            Status = TodoStatus.Open,
+            Priority = TodoPriority.Medium,
+            Deadline = DateTime.Now
+        }).ToArray();
+
+        using var connection = new SQLiteConnection($"Data Source={file}");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "Delete FROM Todos";
+        command.ExecuteNonQuery();
+
+        command.CommandText = "INSERT INTO Todos (Header, Description, Status, Priority, Deadline, Created) VALUES (@Header, @Description, @Status, @Priority, @Deadline, @Created)";
+        command.Connection = connection;
+        command.Parameters.Add("@Header", DbType.String);
+        command.Parameters.Add("@Description", DbType.String);
+        command.Parameters.Add("@Status", DbType.String);
+        command.Parameters.Add("@Priority", DbType.String);
+        command.Parameters.Add("@Deadline", DbType.String);
+        command.Parameters.Add("@Created", DbType.String);
+
+        foreach (var todo in todos)
+        {
+            command.Parameters["@Header"].Value = todo.Header;
+            command.Parameters["@Description"].Value = todo.Description;
+            command.Parameters["@Status"].Value = todo.Status.ToString();
+            command.Parameters["@Priority"].Value = todo.Priority.ToString();
+            command.Parameters["@Deadline"].Value = todo.Deadline?.ToString("yyyy-MM-dd HH:mm") ?? null;
+            command.Parameters["@Created"].Value = todo.CreatedAt.ToString("yyyy-MM-dd HH:mm");
+            command.ExecuteNonQuery();
+        }
     }
 
     [Test]
@@ -27,22 +75,12 @@ public class TodoManagerTests
     [TestCase(1)]
     [TestCase(5)]
     [TestCase(10)]
-    [TestCase(999)]
-    [TestCase(1000)]
-    [TestCase(1001)]
+    [TestCase(99)]
+    [TestCase(100)]
+    [TestCase(101)]
     public void LoadTodos_Test(int count)
     {
-        var todos = Enumerable.Range(0, count).Select(i => new Todo
-        {
-            Id = i,
-            Header = Guid.NewGuid().ToString(),
-            Description = Guid.NewGuid().ToString(),
-            Status = TodoStatus.Open,
-            Priority = TodoPriority.Medium,
-            Deadline = DateTime.Now
-        }).ToArray();
-        var json = JsonSerializer.Serialize(todos, options);
-        File.WriteAllText(file, json);
+        SetupDatasource(count);
 
         var loadedTodos = TodoManager.LoadTodos();
 
@@ -53,26 +91,16 @@ public class TodoManagerTests
     [TestCase(0, 0)]
     [TestCase(1, 0)]
     [TestCase(5, 4)]
-    public void RemoveTodos_Test(int before, int expected)
+    public void RemoveTodos_Test(int before, int expectedCount)
     {
-        var todos = Enumerable.Range(0, before).Select(i => new Todo
-        {
-            Id = i,
-            Header = Guid.NewGuid().ToString(),
-            Description = Guid.NewGuid().ToString(),
-            Status = TodoStatus.Open,
-            Priority = TodoPriority.Medium,
-            Deadline = DateTime.Now
-        }).ToArray();
+        SetupDatasource(before);
 
-        var json = JsonSerializer.Serialize(todos, options);
-        File.WriteAllText(file, json);
-
-        TodoManager.RemoveTodo(0);
+        var removed = TodoManager.RemoveTodo(1);
 
         var loadedTodos = TodoManager.LoadTodos();
 
-        Assert.That(loadedTodos, Has.Length.EqualTo(expected));
+        Assert.That(removed, Is.EqualTo(before != expectedCount)); // if before == expectedCount, then removed should be false
+        Assert.That(loadedTodos, Has.Length.EqualTo(expectedCount));
     }
 
     [Test]
@@ -81,32 +109,22 @@ public class TodoManagerTests
     [TestCase(5, 6)]
     public void AddTodos_Test(int before, int expected)
     {
-        var todos = Enumerable.Range(0, before).Select(i => new Todo
-        {
-            Id = i,
-            Header = Guid.NewGuid().ToString(),
-            Description = Guid.NewGuid().ToString(),
-            Status = TodoStatus.Open,
-            Priority = TodoPriority.Medium,
-            Deadline = DateTime.Now
-        }).ToArray();
-
-        var json = JsonSerializer.Serialize(todos, options);
-        File.WriteAllText(file, json);
+        SetupDatasource(before);
 
         var todo = new Todo
         {
-            Id = -1,
             Header = Guid.NewGuid().ToString(),
             Description = Guid.NewGuid().ToString(),
             Status = TodoStatus.Open,
             Priority = TodoPriority.Medium,
             Deadline = DateTime.Now
         };
-        TodoManager.AddTodo(todo);
+
+        var added = TodoManager.AddTodo(todo);
 
         var loadedTodos = TodoManager.LoadTodos();
 
+        Assert.That(added, Is.True);
         Assert.That(loadedTodos, Has.Length.EqualTo(expected));
         Assert.That(loadedTodos.Last().Id, Is.Not.EqualTo(-1));
     }
@@ -116,23 +134,12 @@ public class TodoManagerTests
     [TestCase(1)]
     [TestCase(5)]
     [TestCase(10)]
-    [TestCase(999)]
-    [TestCase(1000)]
-    [TestCase(1001)]
+    [TestCase(99)]
+    [TestCase(100)]
+    [TestCase(101)]
     public void ExportTodos_Test(int count)
     {
-        var todos = Enumerable.Range(0, count).Select(i => new Todo
-        {
-            Id = i,
-            Header = Guid.NewGuid().ToString(),
-            Description = Guid.NewGuid().ToString(),
-            Status = TodoStatus.Open,
-            Priority = TodoPriority.Medium,
-            Deadline = DateTime.Now
-        }).ToArray();
-
-        var json = JsonSerializer.Serialize(todos, options);
-        File.WriteAllText(file, json);
+        SetupDatasource(count);
 
         var jsonPath = "test.json";
 
